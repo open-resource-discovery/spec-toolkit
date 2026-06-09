747f32edaaa8e4ef7d2da0323c533039bee48dc4
Simon Heimler <simon.heimler@sap.com>
2026-06-09 14:22:44 +0200

    Update dependencies of spec-toolkit and website

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
-import fs from "fs";
 import { pathsToModuleNameMapper } from "ts-jest";
-
-function loadJson(path) {
-  return JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
-}
-
-const tsconfig = loadJson("./tsconfig.json");
-const { compilerOptions } = tsconfig;
 
 export default {
   preset: "ts-jest/presets/default-esm",
@@ -15,8 +7,7 @@ export default {
   collectCoverage: true,
   moduleFileExtensions: ["js", "json", "ts", "d.ts"],
   reporters: ["default"],
-  modulePaths: [compilerOptions.baseUrl],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { useESM: true }),
+  moduleNameMapper: pathsToModuleNameMapper({}),
   watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
   testTimeout: 600000,
   transform: {},
