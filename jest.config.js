250cc6161dcec646873c00119bfe8d7f21d78c20
Simon Heimler <simon.heimler@sap.com>
2026-06-09 14:26:22 +0200

    Fix copy commands in scripts

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-import { pathsToModuleNameMapper } from "ts-jest";
-
 export default {
   preset: "ts-jest/presets/default-esm",
   modulePathIgnorePatterns: ["src/__tests__/generated", "src/generated", "dist"],
@@ -7,7 +5,9 @@ export default {
   collectCoverage: true,
   moduleFileExtensions: ["js", "json", "ts", "d.ts"],
   reporters: ["default"],
-  moduleNameMapper: pathsToModuleNameMapper({}),
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
   testTimeout: 600000,
   transform: {},
