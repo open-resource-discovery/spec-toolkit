d867900c76b9683c6b1a30597f0959019af733fc
Simon Heimler <simon.heimler@sap.com>
2026-09-07 16:38:28 -0700

    chore: update dependencies (#100)

--- src/util/markdownTextHelper.ts
+++ src/util/markdownTextHelper.ts
@@ -1,12 +1,14 @@
 /**
  * Generates a YAML Frontmatter header with common needed meta information
  */
-export function getMarkdownFrontMatter(mdFrontmatter?: { [key: string]: string }): string {
+export function getMarkdownFrontMatter(mdFrontmatter?: { [key: string]: string | undefined }): string {
   let text = "";
   if (mdFrontmatter) {
     text += "---\n";
     for (const [key, value] of Object.entries(mdFrontmatter)) {
-      text += `${key}: "${value}"\n`;
+      if (value !== undefined) {
+        text += `${key}: "${value}"\n`;
+      }
     }
     text += "---\n\n";
   }
